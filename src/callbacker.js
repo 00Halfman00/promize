017322ebc794c73cdc9c0770e4a44176616b3597
Oscar Sanchez <oscarsanchez@Oscars-MacBook-Pro.local>
2020-10-18 12:03:00 -0500

    trucking along

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,4 +1,17 @@
-const syncCallbacker = null;
+
+const func1 = () => {
+    
+}
+
+const func2 = () => {
+    
+}
+
+
+const syncCallbacker = (func1, func2) => {
+    const value = func1();
+    return func2(value);
+};
 
 const asyncCallbacker = null;
 
